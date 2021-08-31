d22324109701fc7e6a6f0df9d977b85c66d29955
michaelsatumba <mjsatumba@gmail.com>
2021-08-31 09:27:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
